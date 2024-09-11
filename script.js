263890eb43b5c683285eabd48f399291a4279a64
GauravJagtap04 <gauravjagtap765@gmail.com>
2024-09-12 01:14:01 +0530

    update

---
+++ script.js
@@ -0,0 +1,53 @@
+const projectsListMain = document.getElementById('projects-list-main');
+const main = document.querySelector('main');
+const nav = document.querySelector('nav');
+const footer = document.querySelector('footer');
+const projectFrame = document.getElementById('projectFrame');
+const projectContainer = document.getElementById('project-container');
+const projectTitle = document.querySelector('.project-tile');
+
+async function loadProject(url) {
+  projectContainer.style.display = 'block';
+  nav.style.filter = 'blur(5px)';
+  main.style.filter = 'blur(5px)';
+  footer.style.filter = 'blur(5px)';
+
+  projectContainer.innerHTML = "";
+  projectContainer.innerHTML = `
+    <img id="close-icon" src="images/x.png" alt="close-img" title="Close" onclick="closeProjectContainer()">
+    <iframe id="projectFrame" src="${url}"></iframe>
+  `;
+} 
+
+function closeProjectContainer() {
+  projectContainer.style.display = 'none';
+  nav.style.filter = 'none';
+  main.style.filter = 'none';
+  footer.style.filter = 'none';
+}
+
+async function showCode(htmlUrl, cssUrl) {
+  projectContainer.style.display = "block";
+  projectContainer.innerHTML = "";
+
+  projectContainer.innerHTML = `
+    <img id="close-icon" src="images/x.png" alt="close-img" title="Close" onclick="closeProjectContainer()">
+    <pre id="codeDisplay" style="display: none; white-space: pre-wrap; word-wrap: break-word;"></pre>
+  `;
+
+  const htmlCode = await fetchFile(htmlUrl);
+  const cssCode = await fetchFile(cssUrl);
+
+  const codeDisplay = document.getElementById('codeDisplay');
+  codeDisplay.textContent = `HTML Code:\n${htmlCode}\n\nCSS Code:\n${cssCode}`;
+  codeDisplay.style.display = 'block';
+}
+
+async function fetchFile(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    return await response.text();
+  } else {
+    return 'Error fetching file.';
+  }
+}
